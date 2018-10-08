447b748b856618291016de8fcb3b4f8b9e9c77ba
= <sam.gilroy2@gmail.com>
2018-10-08 16:32:32 -0500

    started adding degrees to sign up page

--- CSC394Group3/src/app/register/register.component.ts
+++ CSC394Group3/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from '../core/auth.service'
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-register',
@@ -13,19 +15,24 @@ export class RegisterComponent {
   registerForm: FormGroup;
   errorMessage: string = '';
   successMessage: string = '';
+  degrees: Observable<any>;
 
   constructor(
     public authService: AuthService,
     private router: Router,
-    private form: FormBuilder
+    private form: FormBuilder,
+    private db : AngularFirestore
   ) {
+    this.degrees = db.collection("degree").valueChanges();
     this.createForm();
    }
 
    createForm() {
      this.registerForm = this.form.group({
        email: ['', Validators.required ],
-       password: ['',Validators.required]
+       password: ['',Validators.required],
+       job:[''],
+       degree: ['']
      });
    }
 
